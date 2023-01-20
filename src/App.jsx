0ea2b29230ea2a0e0f9ec156dae6f5082aea2546
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2023-01-21 00:55:42 +0530

    Minor Changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,45 +4,42 @@ import { ThemeContext, themes } from "./contexts/ThemeContext";
 import "./index.css";
 
 function App() {
-    const [solution, setSolution] = useState(null);
+  const [solution, setSolution] = useState(null);
 
-    useEffect(() => {
-        fetch(
-            "https://raw.githubusercontent.com/Sandeep-M23/nyt-wordle/main/public/solution.json"
-        )
-            .then((res) => res.json())
-            .then((json) => {
-                const randomSolution =
-                    json[Math.floor(Math.random() * json.length)];
-                setSolution(randomSolution.word);
-            });
-    }, [setSolution]);
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/Sandeep-M23/nyt-wordle/main/public/solution.json"
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        const randomSolution = json[Math.floor(Math.random() * json.length)];
+        setSolution(randomSolution.word);
+      });
+  }, [setSolution]);
 
-    const [darkMode, setDarkMode] = useState(true);
+  const [darkMode, setDarkMode] = useState(true);
 
-    return (
-        <div className="App">
-            <nav>
-                <p>{"   "}</p>
-                <h1>Wordle</h1>
-                <ThemeContext.Consumer>
-                    {({ changeTheme }) => (
-                        <button
-                            onClick={() => {
-                                setDarkMode(!darkMode);
-                                changeTheme(
-                                    darkMode ? themes.light : themes.dark
-                                );
-                            }}
-                        >
-                            {darkMode ? "Dark" : "Light"}
-                        </button>
-                    )}
-                </ThemeContext.Consumer>
-            </nav>
-            {solution && <Wordle solution={solution} />}
-        </div>
-    );
+  return (
+    <div className="App">
+      <nav>
+        <p>{"   "}</p>
+        <h1>Wordle</h1>
+        <ThemeContext.Consumer>
+          {({ changeTheme }) => (
+            <button
+              onClick={() => {
+                setDarkMode(!darkMode);
+                changeTheme(darkMode ? themes.light : themes.dark);
+              }}
+            >
+              {darkMode ? "Dark" : "Light"}
+            </button>
+          )}
+        </ThemeContext.Consumer>
+      </nav>
+      {solution && <Wordle solution={solution} />}
+    </div>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,9 @@ import ThemeContextWrapper from "./contexts/themeContextWrapper";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <ThemeContextWrapper>
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
-    </ThemeContextWrapper>
+  <ThemeContextWrapper>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeContextWrapper>
 );
